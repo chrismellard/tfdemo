787a4f2d2f9d7a6941c608fe596bf2cd97f6e259
Chris Mellard <chris.mellard@icloud.com>
2022-04-11 21:15:03 +1200

    initial commit

---
+++ terraform/app-service.tf
@@ -0,0 +1,25 @@
+resource "azurerm_app_service_plan" "default" {
+  name                = "appserviceplan-pro"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "default" {
+  name                = "cm-app-service-8657"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  app_service_plan_id = azurerm_app_service_plan.default.id
+
+  site_config {
+    linux_fx_version = "DOCKER|nginxdemos/hello"
+  }
+
+
+}
---
+++ terraform/resourcegroups.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "default" {
+  name     = "aks-resource-group"
+  location = "Australia East"
+}
